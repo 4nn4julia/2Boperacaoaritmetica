2dd7c16d9f593c11c2dbb43e23788b3e19488589
anna julia de lima <160244526+4nn4julia@users.noreply.github.com>
2024-06-20 08:14:42 -0300

    carga inicial

---
+++  2Boperacaoaritmetica/script.js
@@ -0,0 +1,18 @@
+//var a = 78;
+//var b = 15;
+
+//var total = a + b;
+//console. log(total);
+
+//var a = 78;
+//var b = 15;
+
+//var total = a - b;
+//console. log (total);
+
+
+//var a = 78;
+//var b = 15;
+
+//var total = a * b;
+//console. long (total)
